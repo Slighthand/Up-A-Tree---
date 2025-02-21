b6507eb53ab9c6fc0b58f3d21240653be9281fa3
Slighthand <adeline.siu251@ycdsbk12.ca>
2025-02-21 10:09:38 -0500

    Update player momvement.cs

--- Assets/Scripts/player momvement.cs
+++ Assets/Scripts/player momvement.cs
@@ -2,8 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playermomvement : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float moveForce = 10f;
+
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private float movementX;
+    
+    private Rigidbody2D myBody;
+
+    private SpriteRenderer sr;
+
+    private Animator anim;
+    private string WALK_ANIMATION = "walk";
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +36,32 @@ public class playermomvement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMoveKeyboard();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+
+    }
+
+    void AnimatePlayer()
+    {
+        if (movementX > 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+        }
+        else if (movementX < 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
     }
 }
