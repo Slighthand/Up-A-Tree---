fee17555c75614106eede0c3a0077234c19f3080
Tarlan <158780016+ajidf@users.noreply.github.com>
2025-02-20 21:01:57 -0500

    Merge branch 'main' of https://github.com/Slighthand/Up-A-Tree---

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    puplic float speed = 5;
+    Private float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        float h = Input.GetAxis("Horizantal");
+        float v = Input.GetAxis("Vertical");
+
+
+        Vector2 pos = transform.position;
+
+        pos.x += h; *Time.deltaTime;
+        pos.y += v; *Time.deltaTime;
+
+        transform.postion = pos;
+
     }
 
     // Update is called once per frame
