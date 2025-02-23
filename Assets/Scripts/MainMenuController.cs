95a918f6284fb111bf4103ed814ef2b2cee1046d
anqiey <144811036+anqiey@users.noreply.github.com>
2025-02-22 22:21:48 -0500

    Added a 'PlayGame' function attached to the Play Game button

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayGame() {
+        Debug.Log("Button is clicked.");
+    }
+} // class
+
+
+
+
+
+
+
+
+
